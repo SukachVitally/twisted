aa3c94d8e5b0955384676cecd986d89e4501620e
Sukach Vitaly <sukach.v.v@gmail.com>
2015-06-01 12:38:36 +0300

    logging

---
+++ loggin/logging_test.py
@@ -0,0 +1,17 @@
+__author__ = 'kent'
+
+import sys
+from twisted.python import log
+
+
+log.startLogging(sys.stdout)
+log.msg("Starting experiment")
+
+log.msg("Logging an exception")
+try:
+    1 / 0
+except ZeroDivisionError, e:
+    log.err(e)
+
+
+log.msg("Ending experiment")
